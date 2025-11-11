8649748846704c07e77f58d2d68bedeaa5c7da94
Oleksandra Bardanova <tirz_hillelcsharp@outlook.com>
2025-11-11 23:22:29 +0100

    Add the class structure

---
+++ DoctorAppointment.Data/Configuration/AppSettings.cs
@@ -0,0 +1,33 @@
+﻿namespace DoctorAppointment.Data.Configuration
+{
+
+    public class AppSettings
+    {
+        public DatabaseSettings Database { get; set; }
+    }
+
+    public class DatabaseSettings
+    {
+        public DoctorsSettings Doctors { get; set; }
+        public PatientsSettings Patients { get; set; }
+        public AppointmentsSettings Appointments { get; set; }
+    }
+
+    public class DoctorsSettings
+    {
+        public int LastId { get; set; }
+        public string Path { get; set; }
+    }
+
+    public class PatientsSettings
+    {
+        public int LastId { get; set; }
+        public string Path { get; set; }
+    }
+
+    public class AppointmentsSettings
+    {
+        public int LastId { get; set; }
+        public string Path { get; set; }
+    }
+}
