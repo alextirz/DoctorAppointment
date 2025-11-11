14f37c3bb2ec33edbd52db5a34b877366ec78c44
Oleksandra Bardanova <tirz_hillelcsharp@outlook.com>
2025-11-12 00:08:30 +0100

    Remove obsolete comments

--- DoctorAppointment.Data/Configuration/Constants.cs
+++ DoctorAppointment.Data/Configuration/Constants.cs
@@ -2,8 +2,6 @@
 {
     public static class Constants
     {
-        // заменить на путь валидный для вашей директории на пк (в будущем будем использовать относительный путь)
-  
         public static string solutionPath = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName;
         public static readonly string AppSettingsPath = $"{solutionPath}\\DoctorAppointment.Data\\Configuration\\appsettings.json";
     }
