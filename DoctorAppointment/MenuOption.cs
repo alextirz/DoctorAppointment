814e17ef0f1702f3bf7f498ac10d4dc7dd72ec8d
Oleksandra Bardanova <tirz_hillelcsharp@outlook.com>
2025-11-11 20:29:05 +0100

    Add menu for adding doctor

---
+++ DoctorAppointment/MenuOption.cs
@@ -0,0 +1,11 @@
+﻿namespace DoctorAppointment.UI
+{
+    internal enum MenuOption
+    {
+        ShowAllDoctors = 1,
+        AddDoctor,
+        Exit,
+        AddPatient,
+        AddAppointment,
+    }
+}
--- DoctorAppointment/Program.cs
+++ DoctorAppointment/Program.cs
@@ -1,4 +1,5 @@
-﻿using MyDoctorAppointment.Domain.Entities;
+﻿using DoctorAppointment.UI;
+using MyDoctorAppointment.Domain.Entities;
 using MyDoctorAppointment.Service.Interfaces;
 using MyDoctorAppointment.Service.Services;
 
@@ -15,39 +16,74 @@ namespace MyDoctorAppointment
 
         public void Menu()
         {
-            //while (true)
-            //{
-            //    // add Enum for menu items and describe menu
-            //}
+            while (true)
+            {
+                // add Enum for menu items and describe menu
+                Console.WriteLine("\n=== Doctor Appointment Menu ===");
+                Console.WriteLine("1. Show all doctors");
+                Console.WriteLine("2. Add new doctor");
+                Console.WriteLine("3. Exit");
+                Console.Write("Choose an option: ");
 
+                if (!int.TryParse(Console.ReadLine(), out int choice) || !Enum.IsDefined(typeof(MenuOption), choice))
+                {
+                    Console.WriteLine("Invalid choice. Try again.");
+                    continue;
+                }
+
+                switch ((MenuOption)choice)
+                {
+                    case MenuOption.ShowAllDoctors:
+                        ShowAllDoctors();
+                        break;
+
+                    case MenuOption.AddDoctor:
+                        AddDoctor();
+                        break;
+
+                    case MenuOption.Exit:
+                        Console.WriteLine("Exiting...");
+                        return;
+                }
+            }
+        }
+
+        private void ShowAllDoctors()
+        {
+            var doctors = _doctorService.GetAll();
+  
             Console.WriteLine("Current doctors list: ");
             var docs = _doctorService.GetAll();
 
-            foreach (var doc in docs)
+            if (!doctors.Any())
             {
-                Console.WriteLine(doc.Name);
+                Console.WriteLine("No doctors found.");
+                return;
             }
 
+            foreach (var doc in docs)
+            {
+                Console.WriteLine($"{doc.Id}. {doc.Name} {doc.Surname} - {doc.DoctorType}, {doc.Experience} years exp.");
+            }
+        }
+
+        private void AddDoctor()
+        {
             Console.WriteLine("Adding doctor: ");
 
             var newDoctor = new Doctor
             {
-                Name = "Vasya",
-                Surname = "Petrov",
-                Experience = 20,
+                Name = "Vasyl",
+                Surname = "Petrenko",
+                Experience = 10,
                 DoctorType = Domain.Enums.DoctorTypes.Dentist
             };
 
             _doctorService.Create(newDoctor);
+            Console.WriteLine($"Doctor {newDoctor.Name} {newDoctor.Surname} added successfully!");
 
-            Console.WriteLine("Current doctors list: ");
-            docs = _doctorService.GetAll();
-
-            foreach (var doc in docs)
-            {
-                Console.WriteLine(doc.Name);
-            }
         }
+
     }
 
     public static class Program
